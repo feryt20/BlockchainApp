73c28289a7caa5f26a3bd1a4522ce5f3a08a0243
farshad kalhor <farshadt20@yahoo.com>
2024-12-13 21:05:27 +0330

    fixed

--- BlockchainApp/Classes/ProofOfStake.cs
+++ BlockchainApp/Classes/ProofOfStake.cs
@@ -1,43 +1,43 @@
-﻿namespace BlockchainApp.Classes
-{
-    public class ProofOfStake
-    {
-        private Dictionary<string, int> Stakes { get; set; }
+﻿//namespace BlockchainApp.Classes
+//{
+//    public class ProofOfStake
+//    {
+//        private Dictionary<string, int> Stakes { get; set; }
 
-        public ProofOfStake()
-        {
-            Stakes = new Dictionary<string, int>();
-        }
+//        public ProofOfStake()
+//        {
+//            Stakes = new Dictionary<string, int>();
+//        }
 
-        public void AddStake(string node, int amount)
-        {
-            if (Stakes.ContainsKey(node))
-            {
-                Stakes[node] += amount;
-            }
-            else
-            {
-                Stakes[node] = amount;
-            }
-        }
+//        public void AddStake(string node, int amount)
+//        {
+//            if (Stakes.ContainsKey(node))
+//            {
+//                Stakes[node] += amount;
+//            }
+//            else
+//            {
+//                Stakes[node] = amount;
+//            }
+//        }
 
-        public string SelectValidator()
-        {
-            int totalStakes = Stakes.Values.Sum();
-            Random random = new Random();
-            int selected = random.Next(totalStakes);
-            int current = 0;
+//        public string SelectValidator()
+//        {
+//            int totalStakes = Stakes.Values.Sum();
+//            Random random = new Random();
+//            int selected = random.Next(totalStakes);
+//            int current = 0;
 
-            foreach (var stake in Stakes)
-            {
-                current += stake.Value;
-                if (current >= selected)
-                {
-                    return stake.Key;
-                }
-            }
+//            foreach (var stake in Stakes)
+//            {
+//                current += stake.Value;
+//                if (current >= selected)
+//                {
+//                    return stake.Key;
+//                }
+//            }
 
-            return null;
-        }
-    }
-}
+//            return null;
+//        }
+//    }
+//}
